d9e1cd231b2e64927c7ef29527d286557c51ac28
Alexander Skvortsov <skvortsov.sasha1997@gmail.com>
2021-03-11 16:43:16 +0300

    Function

--- GameSnake/GameSnake/Program.cs
+++ GameSnake/GameSnake/Program.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameSnake
+namespace Snake
 {
     class Program
     {
+        static void Draws(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symbol);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            int x1 = 3, y1 = 3;
+            char sym1 = '*';
+
+            Draws(x1, y1, sym1);
+
+            int x2 = 4, y2 = 5;
+            char sym2 = '*';
+
+            Draws(x2, y2, sym2);
+
+            Console.ReadLine();
         }
     }
 }
